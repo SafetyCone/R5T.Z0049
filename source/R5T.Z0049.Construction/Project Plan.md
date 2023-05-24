9a54ab2fbc87b416d7f4a0480b61e517d2358940
davidcoats <david@getsafetycone.com>
2023-05-24 12:21:47 -0700

    Initial commit.

---
+++ source/R5T.Z0049.Construction/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.Z0049.Construction
+Construction console project for the R5T.Z0049 library.
---
+++ source/R5T.Z0049/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.Z0049
+{
+	/// <summary>
+	/// Example XML text and object values.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
