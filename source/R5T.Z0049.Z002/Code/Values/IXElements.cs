eee573eb89db86407bd891cbd1de643ed23a4958
davidcoats <david@getsafetycone.com>
2023-07-27 01:32:42 -0700

    Interim changes.

--- source/R5T.Z0049.Z002/Code/Values/IXElements.cs
+++ source/R5T.Z0049.Z002/Code/Values/IXElements.cs
@@ -9,12 +9,15 @@ namespace R5T.Z0049.Z002
     [ValuesMarker]
     public partial interface IXElements : IValuesMarker
     {
+        private static Constructed.IXElements Constructed => Z002.Constructed.XElements.Instance;
         private static Parsed.IXElements Parsed => Z002.Parsed.XElements.Instance;
 
 
-        public XElement Simple => Parsed.Simple;
-        public XElement SummaryWithPara => Parsed.SummaryWithPara;
+        public XElement ChildA => Constructed.ChildA;
+        public XElement Complex_OrderedChildren => Constructed.Complex_OrderedChildren;
         public XElement MemberWithSummaryWithPara => Parsed.MemberWithSummaryWithPara;
         public XElement Misformatted => Parsed.Misformatted;
+        public XElement Simple => Parsed.Simple;
+        public XElement SummaryWithPara => Parsed.SummaryWithPara;
     }
 }
